ohmyzsh/ohmyzsh

Added kgpall alias

plugins/kubectl/kubectl.plugin.zsh
@@ -24,6 +24,7 @@ alias klp='k logs pods'
 alias kep='k edit pods'
 alias kdp='k describe pods'
 alias kdelp='k delete pods'
+alias kgpall='k get pods --all-namespaces -o wide'
 
 # Service management.
 alias kgs='k get svc'

grantstephens/oh-my-zsh

plugins/kubectl/kubectl.plugin.zsh
@@ -43,6 +43,7 @@ alias kgpwide='kgp -o wide'
 alias kep='kubectl edit pods'
 alias kdp='kubectl describe pods'
 alias kdelp='kubectl delete pods'
+alias kgpall='kubectl get pods --all-namespaces -o wide'
 
 # get pod by label: kgpl "app=myapp" -n myns
 alias kgpl='kgp -l'