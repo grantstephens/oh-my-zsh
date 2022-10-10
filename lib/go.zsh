a8484fc7c1b0ded0bfa9625d6aa75fa5af55b367
Grant Stephens <gstephens@fastly.com>
2022-10-10 14:55:37 +0100

    Use the go version

--- lib/go.zsh
+++ lib/go.zsh
@@ -1,7 +1,7 @@
-# get the python version
+# get the go version
 function go_prompt_info() {
   local go_prompt
-  go_prompt=$(goenv global)
+  go_prompt=$(go version | { read _ _ v _; echo ${v#go}; })
   [[ "${go_prompt}x" == "x" ]] && return
   echo "${ZSH_THEME_GO_PROMPT_PREFIX}${go_prompt}${ZSH_THEME_GO_PROMPT_SUFFIX}"
 }
